bb02fef9dac831337c7dcab5bf206f0fe390fda8
chris241 <chrisleon325@gmail.com>
2019-07-29 14:35:38 +0300

    maj2

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,13 +1,14 @@
 require 'gossip'
 class ApplicationController < Sinatra::Base
 	get'/' do
-		erb :index
+		erb :index, locals: {gossips: Gossip.all}
 	end	
 
 	get'/gossips/new' do
 		erb :new_gossip
 		 
 	end
+
 
 	post '/gossips/new/' do
 		Gossip.new(params["gossip_author"],params["gossip_content"]).save
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -10,5 +10,12 @@ class Gossip
     	csv << [@author, @content]
   		end
 	end
+	def self.all
+  		all_gossips = []
+  		CSV.read("./db/gossip.csv").each do |csv_line|
+    	all_gossips << Gossip.new(csv_line[0], csv_line[1])
+  			end
+  		return all_gossips
+	end
 end
 #binding.pry
